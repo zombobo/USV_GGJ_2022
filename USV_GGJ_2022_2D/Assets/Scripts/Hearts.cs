17a0a2846cc5888eae20fb3d58bbf5f7ab882b85
marshallsandovalclark <37922234+marshallsandovalclark@users.noreply.github.com>
2022-01-30 03:13:16 -0800

    Hearts complete

---
+++ USV_GGJ_2022_2D/Assets/Scripts/Hearts.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HEarts : MonoBehaviour
+{
+    public int visibleHearts = 1;
+    // public int health = 1; 
+
+    public Image[] heartMeter;
+    public Sprite fullHeart;
+    public Sprite emptyHeart; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < heartMeter.Length; i++ )
+        {
+            //if(i < health)
+            //{
+            //    heartMeter[i].sprite = fullHeart;
+            //}else
+            //{
+            //    heartMeter[i].sprite = emptyHeart;
+            //}
+
+            if (i < visibleHearts)
+            {
+                heartMeter[i].enabled = true;
+            } else
+            {
+                heartMeter[i].enabled = false;
+            }
+        }
+    }
+}
