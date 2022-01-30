b79ca010c55cb02e1db671e2f4bf300e5b5420f0
Daniel <drsanchez993@gmail.com>
2022-01-29 16:18:55 -0800

    Animation
    animation controller and .anim files

---
+++ USV_GGJ_2022_2D/Assets/Scripts/CharacterAnimation.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private IMove mover;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        mover = GetComponent<IMove>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        float speed = mover.Speed;
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        if (speed != 0)
+            spriteRenderer.flipX = speed > 0;
+    }
+}
---
+++ USV_GGJ_2022_2D/Assets/Scripts/KillOnTouch.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var playerMovementController = collision.collider.GetComponent<PlayerMovementController>();
+        if (playerMovementController != null)
+        {
+            GameManager.Instance.KillPlayer();
+        }
+    }
+}
