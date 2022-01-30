834551b8796d6cc37903372db00124673a36506d
Daniel <drsanchez993@gmail.com>
2022-01-29 22:49:00 -0800

    Main Menu
    Added play, options, and close buttons that WORK!!

--- USV_GGJ_2022_2D/Assets/Scripts/UIStartLevelButton.cs
+++ USV_GGJ_2022_2D/Assets/Scripts/UIStartLevelButton.cs
@@ -12,4 +12,10 @@ public class UIStartLevelButton : MonoBehaviour
     {
         SceneManager.LoadScene(_levelName);
     }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
