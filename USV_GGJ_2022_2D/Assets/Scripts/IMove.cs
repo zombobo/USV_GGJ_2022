b2db940c390079d4e44080336407cb50c534f7dd
marshallsandovalclark <37922234+marshallsandovalclark@users.noreply.github.com>
2022-01-29 23:36:06 -0800

    Fix?

--- USV_GGJ_2022_2D/Assets/Scripts/IMove.cs
+++
@@ -1,4 +0,0 @@
-public interface IMove
-{
-    float vSpeed { get; }
-}
---
+++ USV_GGJ_2022_2D/Assets/Scripts/UIStartLevelButton.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIStartLevelButton : MonoBehaviour
+{
+    [SerializeField] string _levelName;
+
+    public string LevelName => _levelName;
+
+    public void Loadlevel()
+    {
+        SceneManager.LoadScene(_levelName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
